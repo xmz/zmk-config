807fba83d353e5086fc13a0186f90058e02b366e
Mattias Grönberg <me@xmz.se>
2022-04-21 00:23:28 +0200

    Checkpoint

--- build_kyria.sh
+++ build_kyria.sh
@@ -5,4 +5,8 @@ ARGS=""
 
 west build ${ARGS} -s ../app -d .build/left -b nice_nano_v2 -- -DSHIELD=${SHIELD}_left -DZMK_CONFIG=$(pwd)/config
 
+cp -r .build/left/zephyr/zmk.uf2 zmk_${SHIELD}_left.uf2
+
 west build ${ARGS} -s ../app -d .build/right -b nice_nano_v2 -- -DSHIELD=${SHIELD}_right -DZMK_CONFIG=$(pwd)/config
+
+cp -r .build/right/zephyr/zmk.uf2 zmk_${SHIELD}_right.uf2
---
+++ config/swedish-keymap.h
@@ -0,0 +1,148 @@
+/* Copyright 2017 Andreas Lindhé
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+ * │ § │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ + │ ´ │       │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+ * │     │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ Å │ ¨ │     │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
+ * │      │ A │ S │ D │ F │ G │ H │ J │ K │ L │ Ö │ Ä │ ' │    │
+ * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
+ * │    │ < │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ - │          │
+ * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
+ * │    │    │    │                        │    │    │    │    │
+ * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
+ */
+// Row 1
+#define SE_SECT GRAVE       // §
+#define SE_1    NUNBER_1    // 1
+#define SE_2    NUMBER_2    // 2
+#define SE_3    NUMBER_3    // 3
+#define SE_4    NUMBER_4    // 4
+#define SE_5    NUMBER_5    // 5
+#define SE_6    NUMBER_6    // 6
+#define SE_7    NUMBER_7    // 7
+#define SE_8    NUMBER_8    // 8
+#define SE_9    NUMBER_9    // 9
+#define SE_0    NUMBER_0    // 0
+#define SE_PLUS MINUS       // +
+#define SE_ACUT EQUAL       // ´ (dead)
+// Row 2
+#define SE_Q    Q       // Q
+#define SE_W    W       // W
+#define SE_E    E       // E
+#define SE_R    R       // R
+#define SE_T    T       // T
+#define SE_Y    Y       // Y
+#define SE_U    U       // U
+#define SE_I    I       // I
+#define SE_O    O       // O
+#define SE_P    P       // P
+#define SE_AO   LEFT_BRACKET // Å
+#define SE_DIAE RIGHT_BRACKET // ¨ (dead)
+// Row 3
+#define SE_A    A       // A
+#define SE_S    S       // S
+#define SE_D    D       // D
+#define SE_F    F       // F
+#define SE_G    G       // G
+#define SE_H    H       // H
+#define SE_J    J       // J
+#define SE_K    K       // K
+#define SE_L    L       // L
+#define SE_OE   SEMICOLON // Ö - really?
+#define SE_AE   SINGLE_QUOTE // Ä - really?
+#define SE_QUOT NON_US_HASH // '
+// Row 4
+#define SE_LABK NON_US_BACKSLASH // <
+#define SE_Z    Z       // Z
+#define SE_X    X       // X
+#define SE_C    C       // C
+#define SE_V    V       // V
+#define SE_B    B       // B
+#define SE_N    N       // N
+#define SE_M    M       // M
+#define SE_COMM COMMA   // ,
+#define SE_DOT  PERIOD  // .
+#define SE_MINS SLASH   // -
+
+/* Shifted symbols
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+ * │ ½ │ ! │ " │ # │ ¤ │ % │ & │ / │ ( │ ) │ = │ ? │ ` │       │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+ * │     │   │   │   │   │   │   │   │   │   │   │   │ ^ │     │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
+ * │      │   │   │   │   │   │   │   │   │   │   │   │ * │    │
+ * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
+ * │    │ > │   │   │   │   │   │   │   │ ; │ : │ _ │          │
+ * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
+ * │    │    │    │                        │    │    │    │    │
+ * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
+ */
+// Row 1
+#define SE_HALF LS(SE_SECT) // ½
+#define SE_EXLM LS(SE_1)    // !
+#define SE_DQUO LS(SE_2)    // "
+#define SE_HASH LS(SE_3)    // #
+#define SE_CURR LS(SE_4)    // ¤
+#define SE_PERC LS(SE_5)    // %
+#define SE_AMPR LS(SE_6)    // &
+#define SE_SLSH LS(SE_7)    // /
+#define SE_LPRN LS(SE_8)    // (
+#define SE_RPRN LS(SE_9)    // )
+#define SE_EQL  LS(SE_0)    // =
+#define SE_QUES LS(SE_PLUS) // ?
+#define SE_GRV  LS(SE_ACUT) // ` (dead)
+// Row 2
+#define SE_CIRC LS(SE_DIAE) // ^ (dead)
+// Row 3
+#define SE_ASTR LS(SE_QUOT) // *
+// Row 4
+#define SE_RABK LS(SE_LABK) // >
+#define SE_SCLN LS(SE_COMM) // ;
+#define SE_COLN LS(SE_DOT)  // :
+#define SE_UNDS LS(SE_MINS) // _
+
+/* AltGr symbols
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+ * │   │   │ @ │ £ │ $ │ € │   │ { │ [ │ ] │ } │ \ │   │       │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+ * │     │   │   │   │   │   │   │   │   │   │   │   │ ~ │     │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
+ * │      │   │   │   │   │   │   │   │   │   │   │   │   │    │
+ * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
+ * │    │ | │   │   │   │   │   │   │ µ │   │   │   │          │
+ * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
+ * │    │    │    │                        │    │    │    │    │
+ * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
+ */
+// Row 1
+#define SE_AT   RA(SE_2)    // @
+#define SE_PND  RA(SE_3)    // £
+#define SE_DLR  RA(SE_4)    // $
+#define SE_EURO RA(SE_5)    // €
+#define SE_LCBR RA(SE_7)    // {
+#define SE_LBRC RA(SE_8)    // [
+#define SE_RBRC RA(SE_9)    // ]
+#define SE_RCBR RA(SE_0)    // }
+#define SE_BSLS RA(SE_PLUS) // (backslash)
+// Row 2
+#define SE_TILD RA(SE_DIAE) // ~ (dead)
+// Row 4
+#define SE_PIPE RA(SE_LABK) // |
+#define SE_MICR RA(SE_M)    // µ
+
