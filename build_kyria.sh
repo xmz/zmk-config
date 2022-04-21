aaa1097cfb1b92dfe66b8a11d1ced60513d28340
Mattias Grönberg <me@xmz.se>
2022-04-21 21:06:32 +0000

    Working ish

--- build_kyria.sh
+++ build_kyria.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/bash
 
 SHIELD="kyria_rev2"
-ARGS=""
+ARGS="-p"
 
-west build ${ARGS} -s ../app -d .build/left -b nice_nano_v2 -- -DSHIELD=${SHIELD}_left -DZMK_CONFIG=$(pwd)/config
+CONFIG=$(pwd)/config
 
-cp -r .build/left/zephyr/zmk.uf2 zmk_${SHIELD}_left.uf2
+cd ../app
+west build ${ARGS} -d .build/left -b nice_nano_v2 -- -DSHIELD=${SHIELD}_left -DZMK_CONFIG=${CONFIG} && \
+cp -r .build/left/zephyr/zmk.uf2 ${CONFIG}/../zmk_${SHIELD}_left.uf2
 
-west build ${ARGS} -s ../app -d .build/right -b nice_nano_v2 -- -DSHIELD=${SHIELD}_right -DZMK_CONFIG=$(pwd)/config
+west build ${ARGS} -d .build/right -b nice_nano_v2 -- -DSHIELD=${SHIELD}_right -DZMK_CONFIG=${CONFIG} && \
+cp -r .build/right/zephyr/zmk.uf2 ${CONFIG}/../zmk_${SHIELD}_right.uf2
 
-cp -r .build/right/zephyr/zmk.uf2 zmk_${SHIELD}_right.uf2
+cd ${CONFIG}/..
 
-west build -s ../app -d .build/reset -b nice_nano_v2 -- -DSHIELD=settings_reset
-
-cp -r .build/reset/zephyr/zmk.uf2 zmk_settings_reset.uf2
+#west build -s ../app -d .build/reset -b nice_nano_v2 -- -DSHIELD=settings_reset && \
+#cp -r .build/reset/zephyr/zmk.uf2 zmk_settings_reset.uf2
