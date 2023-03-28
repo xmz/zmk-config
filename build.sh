7e9e39626c8cbd8e84c0fbda63a15ad3dff3c919
Mattias Grönberg <me@xmz.se>
2023-03-28 21:38:46 +0200

    Build-script

--- build.sh
+++ build.sh
@@ -8,9 +8,20 @@ then
     SHIELD=$1
 fi
 
-west build ${ARGS} -s ../app -d .build/${SHIELD}_right -b nice_nano_v2 -- -DSHIELD=${SHIELD}_right -DZMK_CONFIG=$(pwd)/${SHIELD} && \
-cp -r .build/${SHIELD}_right/zephyr/zmk.uf2 zmk_${SHIELD}_right.uf2 && \
-west build ${ARGS} -s ../app -d .build/${SHIELD}_left -b nice_nano_v2 -- -DSHIELD=${SHIELD}_left -DZMK_CONFIG=$(pwd)/${SHIELD} && \
-cp -r .build/${SHIELD}_left/zephyr/zmk.uf2 zmk_${SHIELD}_left.uf2
+DIR=${SHIELD}
+
+if [ -d ${DIR} ]
+then
+    if [ -f ${DIR}/.build_vars ]
+    then
+        source ${DIR}/.build_vars
+    fi
+fi
+
+
+west build ${ARGS} -s ../app -d .build/${DIR}_right -b nice_nano_v2 -- -DSHIELD="${SHIELD}_right${SHIELDS}" -DZMK_CONFIG=$(pwd)/${DIR} && \
+cp -r .build/${DIR}_right/zephyr/zmk.uf2 zmk_${DIR}_right.uf2 && \
+west build ${ARGS} -s ../app -d .build/${DIR}_left -b nice_nano_v2 -- -DSHIELD="${SHIELD}_left${SHIELDS}" -DZMK_CONFIG=$(pwd)/${DIR} && \
+cp -r .build/${DIR}_left/zephyr/zmk.uf2 zmk_${DIR}_left.uf2
 
 #west build -s ../../app -d .build/reset -b nice_nano_v2 -- -DSHIELD=settings_reset && cp -r .build/reset/zephyr/zmk.uf2 zmk_settings_reset.uf2
