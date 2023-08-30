66355bc118c64b59c00b138435b1a028f98af5d1
Mattias Grönberg <me@xmz.se>
2023-08-31 01:07:25 +0200

    Adds Bluetooth disconnect behavior and temporary select

--- behaviors/install.sh
+++ behaviors/install.sh
@@ -4,16 +4,20 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 if [ -d app/ ]
 then
+    
+    for f in $(find $SCRIPT_DIR -iname *.diff)
+    do
+        git apply $f
+    done
+
     for f in $(find $SCRIPT_DIR -iname *.yaml)
     do
-        cp $f app/dts/bindings/behaviors/
+        cp -r $f app/dts/bindings/behaviors/
     done
     for f in $(find $SCRIPT_DIR -iname *.c)
     do
-        cp $f app/src/behaviors/
+        cp -r $f app/src/behaviors/
     done
-
-    git apply ${SCRIPT_DIR}/patch.diff
 else
     echo "TODO: Linking"
 fi
